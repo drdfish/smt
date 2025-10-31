651f384908c8ac2ba6f67641f9279c2e02c9c192
gua8gua <1341888475@qq.com>
2025-10-31 19:23:04 +0800

    server调整

--- ServerProject/Server.c
+++ ServerProject/Server.c
@@ -2,6 +2,34 @@
 
 #define TCP_PORT 8080
 #define UDP_PORT 8081
+#define MAX_CLIENTS 4
+#define MAX_SENSOR_DATA 10
+
+my_socket client_sockets[MAX_CLIENTS];
+sensor_data_t* client_sensor_data[MAX_CLIENTS];
+
+void init_client_sockets() {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        client_sockets[i] = -1; // -1 表示该位置没有连接
+    }
+}
+
+int find_empty_slot() {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (client_sockets[i] == -1) {
+            return i;
+        }
+    }
+    return -1;  // 如果没有空位，返回 -1
+}
+
+void remove_client(int index) {
+    if (index >= 0 && index < MAX_CLIENTS) {
+        close(client_sockets[index]);
+        client_sockets[index] = -1;  // 清除套接字
+        printf("客户端套接字 %d 连接已断开\n", index);
+    }
+}
 
 // 处理 TCP 连接
 int handle_tcp_connection(my_socket client_sock) {
@@ -96,11 +124,15 @@ int main(int argc, char* argv[]) {
     if (argc > 2) {
         udp_port = atoi(argv[2]);
     }
+    init_client_sockets();
 
     init_console();
     printf("启动智能家具服务器...\n");
-    if(network_init()<0)
+    init_console();
+    if(network_init()<0) {
+        fprintf(stderr, "网络初始化失败\n");
         return -1;
+    }
     // 创建 TCP 和 UDP 套接字
     int success =0;
     tcp_sock = create_tcp_socket(&success);
@@ -142,6 +174,12 @@ int main(int argc, char* argv[]) {
         FD_SET(tcp_sock, &read_fds);
         FD_SET(udp_sock, &read_fds);
 
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_sockets[i] != -1) {
+                FD_SET(client_sockets[i], &read_fds);
+            }
+        }
+
         int activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
 
         if (activity < 0) {
@@ -154,22 +192,47 @@ int main(int argc, char* argv[]) {
             struct sockaddr_in client_addr;
             socklen_t client_len = sizeof(client_addr);
 
-            int client_sock = accept_connection(tcp_sock, (struct sockaddr*)&client_addr, &client_len);
+            my_socket client_sock = accept_connection(tcp_sock, (struct sockaddr*)&client_addr, &client_len);
             if (client_sock < 0) {
                 perror("接受连接失败");
                 continue;
             }
 
-            printf("新的TCP客户端连接: %s:%d\n",
-                inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+            // 查找空槽位并添加新客户端
+            int empty_slot = find_empty_slot();
+            if (empty_slot == -1) {
+                printf("客户端连接已满，拒绝连接\n");
+                close(client_sock); // 拒绝连接
+            }
+            else {
+                client_sockets[empty_slot] = client_sock;
+                client_sensor_data[empty_slot] = (sensor_data_t*)malloc(sizeof(sensor_data_t) * MAX_SENSOR_DATA);  // 假设最多有 MAX_SENSOR_DATA 个传感器数据
+                if (client_sensor_data[empty_slot] == NULL) {
+                    perror("内存分配失败");
+                    close(client_sock);
+                    client_sockets[empty_slot] = -1;
+                }
+                else {
+                    printf("新的TCP客户端连接: %s:%d\n",
+                            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                }
 
-            handle_tcp_connection(client_sock);
-            close_socket(client_sock);
+            }
         }
 
         // 处理 UDP 数据
         if (FD_ISSET(udp_sock, &read_fds)) {
             handle_udp_data(udp_sock);
+        }
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (client_sockets[i] != -1 && FD_ISSET(client_sockets[i], &read_fds)) {
+                handle_tcp_connection(client_sockets[i]);
+            }
+            else if (client_sockets[i] != -1) {
+                // 如果客户端连接断开，移除它
+                remove_client(i);
+            }
         }
     }
 
